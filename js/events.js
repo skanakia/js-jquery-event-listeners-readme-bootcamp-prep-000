3fcb2a6e3aa6d1379975618ae55c5a0ac57d8375
Sameer Kanakia <skanakia1@gmail.com>
2018-04-02 14:37:12 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,30 @@
-//define functions here
+
+function getIt(){
+  $('p').on("click",alert('Hey!'))
+
+}
+
+function frameIt(){
+  $('img').on('load',function(){
+    $('img')[0].className = 'tasty'
+  } )
+}
+
+function pressIt(){
+  $('input').on('keydown',function(key){
+    if (parseInt(key.which) === 71){
+      alert('You pressed the \'G\' button.')
+    }
+  })
+}
+
+function submitIt(){
+  $('form').on('submit',alert('Your form is going to be submitted now.'))
+}
 
 $(document).ready(function(){
-
-// call functions here
-
+getIt()
+frameIt()
+pressIt()
+submitIt()
 });
